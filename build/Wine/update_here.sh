8afbf5ef028645009f2c9a036dc147cf2c5f4de6
skl.progs <skl.progs@gmail.com>
2018-09-22 14:02:14 +0300

    Build scripts (Wine)

--- build/Wine/update_here.sh
+++ build/Wine/update_here.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Do not use "verbose" in order to spot errors easily
 
@@ -6,23 +6,23 @@ mkdir -p ./resources/locale/ru/LC_MESSAGES/
 mkdir ./user
 
 # Copy shared resources
-cp -u /usr/local/bin/shared/resources/{error.gif,info.gif,question.gif,warning.gif} ./resources/
+cp -u $HOME/bin/shared/resources/{error.gif,info.gif,question.gif,warning.gif} ./resources/
 
 # Copy other SendTableSymbols resources
-cp -u /usr/local/bin/SendTableSymbols/resources/locale/ru/LC_MESSAGES/SendTableSymbols.mo ./resources/locale/ru/LC_MESSAGES/
-cp -u /usr/local/bin/SendTableSymbols/resources/third\ parties ./resources/
-cp -u /usr/local/bin/SendTableSymbols/user/SendTableSymbols.cfg ./user/
+cp -u $HOME/bin/SendTableSymbols/resources/locale/ru/LC_MESSAGES/SendTableSymbols.mo ./resources/locale/ru/LC_MESSAGES/
+cp -u $HOME/bin/SendTableSymbols/resources/third\ parties ./resources/
+cp -u $HOME/bin/SendTableSymbols/user/SendTableSymbols.cfg ./user/
 
 # Copy SendTableSymbols Python files
-cp -u /usr/local/bin/SendTableSymbols/src/{sendkeysctypes,SendTableSymbols}.py .
+cp -u $HOME/bin/SendTableSymbols/src/{sendkeysctypes,SendTableSymbols}.py .
 
 # Copy shared Python files
-cp -u /usr/local/bin/shared/src/{gettext_windows.py,shared.py,sharedGUI.py} .
+cp -u $HOME/bin/shared/src/{gettext_windows.py,shared.py,sharedGUI.py} .
 
 # (Wine-only) Copy SendTableSymbols icon
 cp -ru /home/pete/bin/SendTableSymbols/resources/SendTableSymbols.ico ./resources/
 
 # (Wine-only) Copy build scripts
-cp -u /usr/local/bin/SendTableSymbols/build/Wine/{build.sh,clean_up.sh,SendTableSymbols.cmd,setup.py} .
+cp -u $HOME/bin/SendTableSymbols/build/Wine/{build.sh,clean_up.sh,SendTableSymbols.cmd,setup.py} .
 
 ls .
